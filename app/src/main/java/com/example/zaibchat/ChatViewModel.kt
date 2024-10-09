ee6db35cec28f74ee39d63bdfd11d2ab99de9c59
Aurangzaib <zaibbuzdar3@gmail.com>
2024-10-09 21:25:47 +0500

    Initial commit

---
+++ app/src/main/java/com/example/zaibchat/ChatViewModel.kt
@@ -0,0 +1,67 @@
+package com.example.zaibchat
+
+import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.content
+import kotlinx.coroutines.launch
+
+class ChatViewModel : ViewModel() {
+
+    val messageList by lazy {
+        mutableStateListOf<MessageModel>()
+    }
+
+    private val generativeModel: GenerativeModel = GenerativeModel(
+        modelName = "gemini-pro",
+        apiKey = Constants.apiKey
+    )
+
+    fun sendMessage(question: String) {
+        val myQuestion = question.trim()
+
+        viewModelScope.launch {
+
+            try {
+
+                val chat = generativeModel.startChat(
+                    history = messageList.map {
+                        content(it.role) { text(it.message) }
+                    }.toList()
+                )
+
+                if (myQuestion == "who is your creator ".trim() || myQuestion == "who created you ".trim()) {
+                    messageList.add(MessageModel(myQuestion, "user"))
+                    messageList.add(MessageModel("Typing....", "model"))
+
+                    val response = chat.sendMessage(myQuestion)
+                    messageList.removeLast()
+                    messageList.add(
+                        MessageModel(
+                            " Aurangzaib Baloch created me using kotlin technology",
+                            "model"
+                        )
+                    )
+
+                } else {
+                    messageList.add(MessageModel(myQuestion, "user"))
+                    messageList.add(MessageModel("Typing....", "model"))
+
+                    val response = chat.sendMessage(myQuestion)
+                    messageList.removeLast()
+                    messageList.add(MessageModel(response.text.toString(), "model"))
+
+                }
+
+
+            } catch (e: Exception) {
+                messageList.removeLast()
+                messageList.add(MessageModel("Error : " + e.message.toString(), "model"))
+            }
+
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/zaibchat/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.zaibchat
+
+object Constants {
+    val apiKey = "AIzaSyDpyu4cXcGU9_cTIXO8ooSa2Fw_M_urOVs"
+}
